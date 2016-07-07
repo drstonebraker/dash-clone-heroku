188b41cddd80e96725952bb1a6b9f5dd943a908a
Drew Stonebraker <Drew Stonebraker>
2016-07-06 20:42:30 -0600

    animate circles with client data

--- js/main.js
+++ js/main.js
@@ -10,55 +10,62 @@ $( document ).ready(function() {
 });
 
 
-
-//USE THESE TO CONFIGURE DASHBOARD FOR INDIVIDUAL CLIENT
-var startingMonth = 1;
-var monthlyHours = 6;
-var monthlyBal = {
-  1: 0,
-  2: 1,
-  3: 2,
-  4: 3,
-  5: 4,
-  6: 5,
-  7: 6,
-  8: 0,
-  9: 0,
-  10: 0,
-  11: 0,
-  12: 0
-};
-var annualHours=36;
-var annualBal=16;
-var today = new Date();
-var currentMonth = today.getMonth() + 1;
-
-var circle = new ProgressBar.Circle('#month' + startingMonth, {
-    strokeWidth: 15,
-    color: '#9bd500',
-    duration: 1300,
-    easing: 'easeOut',
-    trailColor: '#dfdfdf',
-    trailWidth: 15,
-    text: {
-      value: "0",
-      className: 'month__bal',
-      style: null,
-    },
-    step: function(state, circle) {
-
-      var value = Math.round(circle.value() * monthlyHours);
-      if (value === 0) {
-        circle.setText('0');
-      } else {
-        circle.setText(value);
-      }
-
-    }
-});
-
-//Animate progress bars
 window.onload = function onLoad() {
+  //USE THESE TO CONFIGURE DASHBOARD FOR INDIVIDUAL CLIENT
+  var startingMonth = 0; //jan=0, feb=1...
+  var monthlyHours = 6;
+  var monthlyBal = {
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0,
+    11: 0
+  };
+  var annualHours=36;
+  var annualBal=16;
+  var today = new Date();
+  var currentMonth = today.getMonth();
 
-    circle.animate(1);
+  function updateMonths (i, val) {
+    var circle = new ProgressBar.Circle('#month' + i, {
+        strokeWidth: 15,
+        color: '#9bd500',
+        duration: 1300,
+        easing: 'easeOut',
+        trailColor: '#dfdfdf',
+        trailWidth: 15,
+        text: {
+          value: "0",
+          className: 'month__bal',
+          style: null,
+        },
+        step: function(state, circle) {
+
+          var value = Math.round(circle.value() * monthlyHours);
+          if (value === 0) {
+            circle.setText('0');
+          } else {
+            circle.setText(value);
+          }
+
+        }
+    });
+
+
+    //set monthlyBalIteration to appropriate month based on client's starting month
+    var monthlyBalIteration = (startingMonth + i) < 12 ? startingMonth + i : startingMonth + i - 12;
+
+    //Animate circular progress bars
+    circle.animate(monthlyBal[monthlyBalIteration]/monthlyHours);
+  }
+
+  $.each($('.month'), updateMonths)
+
 };
