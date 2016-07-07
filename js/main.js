a10b02ba90cb89878ed7c97d23c9e73244a9678b
Drew Stonebraker <Drew Stonebraker>
2016-07-06 19:42:15 -0600

    set DL link, logo, and progress labels (mobile)

--- js/main.js
+++ js/main.js
@@ -10,44 +10,55 @@ $( document ).ready(function() {
 });
 
 
+
+//USE THESE TO CONFIGURE DASHBOARD FOR INDIVIDUAL CLIENT
+var startingMonth = 1;
+var monthlyHours = 6;
+var monthlyBal = {
+  1: 0,
+  2: 1,
+  3: 2,
+  4: 3,
+  5: 4,
+  6: 5,
+  7: 6,
+  8: 0,
+  9: 0,
+  10: 0,
+  11: 0,
+  12: 0
+};
+var annualHours=36;
+var annualBal=16;
+var today = new Date();
+var currentMonth = today.getMonth() + 1;
+
+var circle = new ProgressBar.Circle('#month' + startingMonth, {
+    strokeWidth: 15,
+    color: '#9bd500',
+    duration: 1300,
+    easing: 'easeOut',
+    trailColor: '#dfdfdf',
+    trailWidth: 15,
+    text: {
+      value: "0",
+      className: 'month__bal',
+      style: null,
+    },
+    step: function(state, circle) {
+
+      var value = Math.round(circle.value() * monthlyHours);
+      if (value === 0) {
+        circle.setText('0');
+      } else {
+        circle.setText(value);
+      }
+
+    }
+});
+
 //Animate progress bars
 window.onload = function onLoad() {
-  //USE THESE TO CONFIGURE DASHBOARD FOR INDIVIDUAL CLIENT
-  var startingMonth = 1;
-  var monthlyHours = 6;
-  var monthlyBal = {
-    jan: 0,
-    feb: 1,
-    mar: 2,
-    apr: 3,
-    may: 4,
-    jun: 5,
-    jul: 6,
-    aug: 0,
-    sep: 0,
-    oct: 0,
-    nov: 0,
-    dec: 0
-  };
-  var annualHours=36;
-  var annualBal=16;
 
-
-
-
-    var circle = new ProgressBar.Circle('#progress', {
-        strokeWidth: 15,
-        color: '#9bd500',
-        duration: 1300,
-        easing: 'easeOut',
-        trailColor: '#dfdfdf',
-        trailWidth: 15,
-        text: {
-          value: "0",
-          className: 'month__bal',
-          style: null,
-        }
-    });
-
-    circle.animate(.75);
+    circle.animate(1);
 };
